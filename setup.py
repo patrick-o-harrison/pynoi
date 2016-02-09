dc9117d035e803292ba0ea27b623a7ba396d6e8c
Patrick Harrison <patrick.o.harrison@gmail.com>
2016-02-09 11:42:49 -0600

    Initial project structure.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pynoi',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/patrick-o-harrison/pynoi',
+    license='Apache-2.0',
+    author='Patrick Harrison',
+    author_email='patrick.o.harrison@gmail.com',
+    description='A component-oriented, middleware-driven web framework.'
+)
